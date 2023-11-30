bfa152599e3d05776f7884c4222bd10c725858b4
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-11-30 09:40:41 -0800

    [TESTING] Files for testing robotica functionality

---
+++ integration.py
@@ -0,0 +1,22 @@
+from robot_launch import Robot
+import time
+
+yml_path = "/home/drojas/robot_arm/robotica/2_link_robot.yml"
+robot = Robot(yml_path)
+
+#joint_traj = [(0.4, 0.13), (0.4, 0.23), (0.4, 0.33), (0.4, 0.43), (0.4, 0.53), (0.4, 0.63), (0.4, 0.73), (0.3, 0.73), (0.2, 0.73), (0, 0.73), (-0.2, 0.73)]
+#joint_traj = [(0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.1, 0.0), (0.2, 0.0), (0.1, 0.0), (0.0, 0.0), (0.0, 0.0)]
+#robot.joint_move(joint_traj)
+
+robot.cartesian_move((0.49, 0.24), (0.35, 0.0), 0.1)
+robot.cartesian_move((0.35, 0.0), (0.24, 0.24), 0.3)
+robot.cartesian_move( (0.24, 0.24), (0.49, 0.24), 0.9)
+
+
+robot.cartesian_move((0.49, 0.24), (0.24, 0.24), 0.1)
+robot.cartesian_move((0.24, 0.24), (0.35, 0.0), 0.5)
+robot.cartesian_move((0.35, 0.0), (0.49, 0.24), 0.9)
+
+
+
+
