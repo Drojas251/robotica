a524bd83194838286b2f79e2a4dfa1185d0a7725
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-04 23:01:17 -0800

    [Feature] Created Visualization and Execution Modules for Sim
    Created separate classes to handle visualization and execution
    of trajectories.

--- robotica_core/robotica_core/simulation/sim_env.py
+++ robotica_core/robotica_core/simulation/sim_env.py
@@ -10,40 +10,54 @@ from robotica_core.utils.yml_parser import load_dh_params
 from robotica_core.kinematics.robot_model import DH_parameters
 from robotica_core.trajectory_planning.trajectory import Trajectory
 
-
-# def update(frame, data_socket, arm_line):
-#     try:
-#         joint_data = data_socket.recv_string().split()  # Extract joint_data from the received message
-#         theta1 = float(joint_data[1])
-#         theta2 = float(joint_data[2])
-
-#         link_length = 1.0
-
-#         x_end = link_length * np.sin(theta1) + link_length * np.sin(theta1 + theta2)
-#         y_end = link_length * np.cos(theta1) + link_length * np.cos(theta1 + theta2)
-
-#         arm_line.set_data([0, link_length * np.sin(theta1), x_end], [0, -link_length * np.cos(theta1), -y_end])
-
-#     except (zmq.error.Again, ValueError) as e:
-#         print(f"{e}")
-#         pass
-
-
-class SimEnv():
+class Visualization():
     def __init__(self, DH_params):
 
         self.plt = plt
-        self.fig, ax = self.plt.subplots()
+        self.figure, ax = self.plt.subplots()
         ax.set_xlim(-1, 1)
         ax.set_ylim(-1, 1)
+
+        # Arm Visual
         self.arm_line, = ax.plot([], [], lw=5)
+
+        # Task Space Path Visual
         self.path, = ax.plot([], [], lw=2)
 
-        # context = zmq.Context()
-        # self.data_socket = context.socket(zmq.SUB)
-        # self.data_socket.connect("tcp://127.0.0.1:5555")  # Use the same address as the publisher
-        # self.data_socket.setsockopt_string(zmq.SUBSCRIBE, "joint_data")
+        # Robot Init Params
+        self.DH_params = DH_params
+        self.link_length_1 = self.DH_params.a[0]
+        self.link_length_2 = self.DH_params.a[1]
 
+    def run(self):
+        self.plt.show()
+
+    def init_space(self):
+        theta1 = self.DH_params.theta[0]
+        theta2 = self.DH_params.theta[1]
+        self.visualize_arm(theta1, theta2)
+
+    def visualize_arm(self, theta1, theta2):
+        y_end = self.link_length_1 * np.sin(theta1) + self.link_length_2 * np.sin(theta1 + theta2)
+        x_end = self.link_length_1 * np.cos(theta1) + self.link_length_2 * np.cos(theta1 + theta2)
+
+        # [origin_x, x_pos_first_joint, x_pos_ee], [origin.y, y_pos_first_joint, y_pos_ee]
+        self.arm_line.set_data([0, self.link_length_1 * np.cos(theta1), x_end], [0, self.link_length_1 * np.sin(theta1), y_end])
+
+    def visualize_cartesian_path(self, cartesian_traj):
+        self._format_path_data(cartesian_traj)
+        self.path.set_data(self.x, self.y)
+
+    def _format_path_data(self, cartesian_traj):
+        self.x = [p[0] for p in cartesian_traj]
+        self.y = [p[1] for p in cartesian_traj]
+
+
+class ExecutionManager():
+    def __init__(self):
+        self.current_trajectory = Trajectory()
+
+        # Joint Service 
         context = zmq.Context()
         self.data_socket = context.socket(zmq.REP)
         self.data_socket.bind("tcp://127.0.0.1:5555")
@@ -51,64 +65,56 @@ class SimEnv():
         self.poller = zmq.Poller()
         self.poller.register(self.data_socket, zmq.POLLIN)
 
-        self.link_length_1 = DH_params.a[0]
-        self.link_length_2 = DH_params.a[1]
-        self.trajectory = Trajectory()
-        self.x = []
-        self.y = []
+    def consume_wpt(self):
+        wpt = self.current_trajectory.joint_traj.pop(0)
 
-    def init_animation(self):
-        theta1 = 0.4
-        theta2 = 0.2
+        if len(self.current_trajectory.joint_traj) == 0:
+            self.data_socket.send_string("Move Completed")
+            
+        return wpt
+    
+    def is_executing_path(self):
+        if len(self.current_trajectory.joint_traj) > 0:
+            return True
+        else:
+            return False
         
-        y_end = self.link_length_1 * np.sin(theta1) + self.link_length_2 * np.sin(theta1 + theta2)
-        x_end = self.link_length_1 * np.cos(theta1) + self.link_length_2 * np.cos(theta1 + theta2)
+    def wait_for_joint_trajectory(self):
+        if self.poller.poll(1000):
+            serialized_data = self.data_socket.recv()
+            self.current_trajectory = pickle.loads(serialized_data)
+            # self.data_socket.send_string("Data received successfully") 
+            return self.current_trajectory.cartesian_traj
+        else:
+            return None
 
-        # [origin_x, x_pos_first_joint, x_pos_ee], [origin.y, y_pos_first_joint, y_pos_ee]
-        self.arm_line.set_data([0, self.link_length_1 * np.cos(theta1), x_end], [0, self.link_length_1 * np.sin(theta1), y_end])
+class SimEnv():
+    def __init__(self, DH_params):
 
-    def plot_path(self):
-        #self.plt.plot(self.x, self.y, color='red', marker='o', markersize=5)
-        self.path.set_data(self.x, self.y)
-
-    def format_path_data(self):
-        self.x = [p[0] for p in self.trajectory.cartesian_traj]
-        self.y = [p[1] for p in self.trajectory.cartesian_traj]
+        self.vis_scene = Visualization(DH_params)
+        self.execution_manager = ExecutionManager()
 
     def run(self):
-        self.animation = FuncAnimation(self.fig, self.update, init_func=self.init_animation, frames=np.arange(0, 10000, 1), interval=50, blit=False)
-        self.plt.show()
-
-    def consume_wpt(self):
-        wpt = self.trajectory.joint_traj.pop(0)
-        return wpt
+        self.animation = FuncAnimation(self.vis_scene.figure, self.update, init_func=self.vis_scene.init_space, frames=np.arange(0, 10000, 1), interval=10, blit=False)
+        self.vis_scene.run()
         
     def update(self, frame):
         try:
-            if self.poller.poll(100):
-                serialized_data = self.data_socket.recv()
-                self.trajectory = pickle.loads(serialized_data)
-                self.data_socket.send_string("Data received successfully") 
-                self.format_path_data()
-                self.plot_path() 
+            if not self.execution_manager.is_executing_path():
+                traj = self.execution_manager.wait_for_joint_trajectory()
+                if traj:
+                    self.vis_scene.visualize_cartesian_path(traj)
+                else:
+                    return
 
-            if len(self.trajectory.joint_traj) == 0:
-                return
-
-            theta1, theta2 = self.consume_wpt()
+            theta1, theta2 = self.execution_manager.consume_wpt()
             print(theta1, theta2)
 
-            y_end = self.link_length_1 * np.sin(theta1) + self.link_length_2 * np.sin(theta1 + theta2)
-            x_end = self.link_length_1 * np.cos(theta1) + self.link_length_2 * np.cos(theta1 + theta2)
-
-            self.arm_line.set_data([0, self.link_length_1 * np.cos(theta1), x_end], [0, self.link_length_1 * np.sin(theta1), y_end])
-
-            time.sleep(0.5)
+            self.vis_scene.visualize_arm(theta1, theta2)
 
         except (zmq.error.Again, ValueError) as e:
             print(f"{e}")
             pass
-
 
 
 if __name__ == "__main__":
