3bff8eb6c9667179fe624fbbaacf1f0df96e946b
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-06 01:09:48 -0800

    [Feature] Pub/Sub base classes

---
+++ robotica_core/robotica_core/utils/robotica_networking.py
@@ -0,0 +1,38 @@
+import zmq
+import threading
+import pickle
+
+class RoboticaPublisher:
+    def __init__(self, port, topic):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.bind(f"tcp://*:{port}")
+        self.topic = topic
+
+    def publish(self, data):
+        #pickled_data = pickle.dumps(data)
+        msg = { f"{self.topic}": data }
+        self.socket.send_json(msg)
+        #self.socket.send_string(f"{self.topic} {pickled_data}")
+
+class RoboticaSubscriber:
+    def __init__(self, port, topic):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect(f"tcp://localhost:{port}")
+        #self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self.socket.setsockopt(zmq.SUBSCRIBE, b"")
+
+        self.topic = topic
+
+    def subscribe(self, callback):
+        threading.Thread(target=self._subscribe, args=(self.topic, callback)).start()
+
+    def _subscribe(self, topic, callback):
+        while True:
+            msg = self.socket.recv_json()
+
+            if topic in msg:
+                data = msg[self.topic]
+                callback(data)
+
