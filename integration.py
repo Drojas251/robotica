7f4b7ccc80430ce1664962f2f2b998ce3ba8813b
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-09 00:01:48 -0800

    Testing

--- integration.py
+++ integration.py
@@ -1,19 +1,29 @@
 from robotica_plugins.robot_launch import Robot
 import time
+from robotica_core.trajectory_planning.cartesian_trajectory_base import WayPoint
+
 
 yml_path = "/home/drojas/robot_arm/robotica/2_link_robot.yml"
 robot = Robot(yml_path)
 
 #joint_traj = [(0.4, 0.13), (0.4, 0.23), (0.4, 0.33), (0.4, 0.43), (0.4, 0.53), (0.4, 0.63), (0.4, 0.73), (0.3, 0.73), (0.2, 0.73), (0, 0.73), (-0.2, 0.73)]
-#joint_traj = [(0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.1, 0.0), (0.2, 0.0), (0.1, 0.0), (0.0, 0.0), (0.0, 0.0)]
-#robot.joint_move(joint_traj)
+# joint_traj = [(0.1, 0.0), (0.2, 0.3)]
+# robot.joint_move(joint_traj)
 
-robot.cartesian_move((0.49, 0.24), (0.35, 0.0), 0.1)
-robot.cartesian_move((0.35, 0.0), (0.24, 0.24), 0.3)
-robot.cartesian_move( (0.24, 0.24), (0.49, 0.24), 0.9)
+# robot.cartesian_move((0.49, 0.24), (0.35, 0.0), 0.1)
+# robot.cartesian_move((0.35, 0.0), (0.24, 0.24), 0.3)
 
-path = [(0.35, 0.0), (0.24, 0.24), (0.49, 0.24)]
-robot.execute_path(path, 0.4)
+#robot.cartesian_move( (0.24, 0.24), (0.49, 0.24), 0.9)
+
+path1 = [WayPoint((0.24, -0.24), 0.3)]
+robot.execute_path(path1)
+
+path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
+robot.execute_path(path)
+
+
+path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, -0.24), 0.2), WayPoint((0.09, 0.24), 0.6)]
+robot.execute_path(path)
 # robot.cartesian_move((0.49, 0.24), (0.24, 0.24), 0.1)
 # robot.cartesian_move((0.24, 0.24), (0.35, 0.0), 0.5)
 # robot.cartesian_move((0.35, 0.0), (0.49, 0.24), 0.9)
