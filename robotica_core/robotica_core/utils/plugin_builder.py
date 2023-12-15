f3685a5f2c58d9ea600f36f768472b730a5a3ae2
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-15 00:42:08 -0800

    [Refactor] Build Plugins and write data to sharable file
    The purpose of this refactor is to be able to write the plugin data
    to a file in order for the sim_app to read it and load the data
    into the GUI.

--- robotica_core/robotica_core/utils/plugin_builder.py
+++ robotica_core/robotica_core/utils/plugin_builder.py
@@ -2,12 +2,14 @@
 
 import os
 import ast
+import yaml
 
 
-class PluginLoader:
+class Plugin:
     MODULE = "robotica_plugins"
     PLUGIN_DIR = "plugins"
     PLUGIN_DEF = "definition.py"
+
     def __init__(self, robotica_plugin_dir, group_name):
         """
         Args:
@@ -25,12 +27,10 @@ class PluginLoader:
         self.imports = []
         self.cls_names = []
 
-        self.generate_definition_file()
+        self._extract_plugin_info()
 
     def generate_definition_file(self):
-
         print(f"\nBuilding Plugins in {self.full_path}")
-        self._extract_plugin_info()
 
         file_content = ""
         for class_name, import_statement in zip(self.cls_names, self.imports):
@@ -71,6 +71,39 @@ class PluginLoader:
             tree = ast.parse(file.read(), filename=file_path)
 
         class_names = [node.name for node in ast.walk(tree) if isinstance(node, ast.ClassDef)]
-        return class_names
+        return class_names        
     
-    
+
+class PluginBuilder:
+    SHARED_DIR = "~/.robotica"
+    PLUGIN_FILE_PATH = "~/.robotica/plugins"
+
+    def __init__(self, plugins):
+        """
+        Args:
+            robotica_plugin_dir (str): Directory name of specific plugin type
+            group_name (str): Plugin in group name
+        """
+        self.plugins = plugins
+        self.generate_plugin_definitions()
+
+        self.shared_plugin_path = os.path.expanduser(self.PLUGIN_FILE_PATH)
+        if not os.path.exists(self.shared_plugin_path):
+            os.makedirs(self.shared_plugin_path)
+
+        self.write_plugins_to_file()
+
+    def generate_plugin_definitions(self):
+        for plugin in self.plugins:
+            plugin.generate_definition_file()
+
+    def write_plugins_to_file(self):
+        data = {}
+        for plugin in self.plugins:
+            data[plugin.group_name] = plugin.cls_names
+
+        yaml_file_path = os.path.join(self.shared_plugin_path, "plugins.yml")
+
+        # Write data to the YAML file
+        with open(yaml_file_path, "w") as yaml_file:
+            yaml.dump(data, yaml_file, default_flow_style=False)
--- robotica_plugins/robotica_plugins/build_plugins.py
+++ robotica_plugins/robotica_plugins/build_plugins.py
@@ -4,13 +4,15 @@ THIS FILE MUST BE IN robotica_plugins/robotica_plugins for the plugin builder to
 """
 
 import os
-from robotica_core.utils.plugin_loader import PluginLoader
+from robotica_core.utils.plugin_builder import Plugin, PluginBuilder
 
 # Get path to plugin in directory 
 plugin_path = os.getcwd()
 os.environ["ROBOTICA_PLUGIN_PATH"] = plugin_path
 
 # Build Plugins
-PluginLoader(robotica_plugin_dir="kinematics", group_name="Kinematics_Plugins")
-PluginLoader(robotica_plugin_dir="trajectory_planners", group_name="Trajectory_Planners_Plugins")
-PluginLoader(robotica_plugin_dir="path_planners", group_name="Path_Planners_Plugins")
+kinematics_plugin = Plugin(robotica_plugin_dir="kinematics", group_name="Kinematics_Plugins")
+traj_planner_plugin = Plugin(robotica_plugin_dir="trajectory_planners", group_name="Trajectory_Planners_Plugins")
+path_planner_plugin = Plugin(robotica_plugin_dir="path_planners", group_name="Path_Planners_Plugins")
+
+PluginBuilder([kinematics_plugin, traj_planner_plugin, path_planner_plugin])
