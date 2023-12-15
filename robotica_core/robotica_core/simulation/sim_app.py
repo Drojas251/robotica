16a2036c1b31ed8d2fafdf3513bcdc670781142a
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-15 00:50:42 -0800

    [Feature] Dynamically generate plugin drop down menus
    The shared file that was written to by the plugin builder is read
    in the app and used to populate the drop down menus for plugin options.

--- robotica_core/robotica_core/simulation/sim_app.py
+++ robotica_core/robotica_core/simulation/sim_app.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import sys
+import os
+import yaml
 
 from robotica_core.utils.yml_parser import RobotParamsLoader
 from robotica_core.utils.robotica_networking import RoboticaPublisher
 from robotica_core.control.controller_manager import ControllerManager
 from robotica_datatypes.kinematic_datatypes.DH_params import DH_parameters    
-
-import random
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
@@ -59,8 +59,33 @@ class Visualization():
         self.y = [p.point[1] for p in cartesian_traj]
 
 
+class PluginOption:
+    def __init__(self, master, group_name, plugins):
+        self.master = master
+        self.group_name = group_name
+        self.plugins = plugins
+
+        self.label = None
+        self.dropdown = None
+        self.plugin_selected = "None"
+
+    def make_label(self, row, col):
+        self.label = Label(self.master, text=self.group_name, width=25)
+        self.label.grid(row=row, column=col)
+
+    def make_drop_down(self, row, col):
+        if len(self.plugins) > 0:
+            self.plugin_selected = tk.StringVar(value=self.plugins[0])
+        else:
+            self.plugin_selected = tk.StringVar(value="None")
+
+        self.dropdown = ttk.Combobox(self.master, textvariable=self.plugin_selected, values=self.plugins, state="readonly")
+        self.dropdown.grid(row=row, column=col)
 
 class SimApp(Frame):
+    PLUGIN_FILE_PATH = "~/.robotica/plugins"
+    SELECTED_PLUGIN_FILE = "run_plugins.yml"
+
     def __init__(self, DH_params, master = None):
         Frame.__init__(self, master)
         self.master = master
@@ -71,15 +96,14 @@ class SimApp(Frame):
         self.curr_joints = DH_params.theta
         self.sim_interval = 0.01
 
-        self.init_window()
+        # Plugins
+        self.plugin_dict = {}
+        self.plugin_menu = {}
+        self._read_plugin_data()
+        
+        self._init_gui()
 
-    def Clear(self):      
-        print(f"{self.selected_option.get()}")
-
-    def Plot(self):
-        pass
-
-    def update(self, frame):
+    def _update(self, frame):
         if not self.controller_manager.is_executing_path():
             traj = self.controller_manager.wait_for_joint_trajectory(1000)
             if traj:
@@ -98,52 +122,56 @@ class SimApp(Frame):
         self.curr_joints = (theta1, theta2)
         self._publish_joints()
 
-    def init_window(self):
+    def _set_plugins(self):
+        run_plugins = {}
+        for group_name, plugin_options in self.plugin_menu.items():
+            run_plugins[group_name] = plugin_options.plugin_selected.get()
+            print(f"{group_name}: {plugin_options.plugin_selected.get()}")
+
+        shared_plugin_path = os.path.expanduser(self.PLUGIN_FILE_PATH)
+        yaml_file_path = os.path.join(shared_plugin_path, self.SELECTED_PLUGIN_FILE)
+
+        with open(yaml_file_path, "w") as yaml_file:
+            yaml.dump(run_plugins, yaml_file, default_flow_style=False)
+
+    def _init_gui(self):
         self.master.title("Use Of FuncAnimation in tkinter based GUI")
         self.pack(fill='both', expand=1)     
 
-        #Create the controls, note use of grid
+        self.set_plugins_button = Button(self,text="Set Plugins",command=self._set_plugins,width=12)
+        self.set_plugins_button.grid(row=2,column=1)
+        self.set_plugins_button.bind(lambda e:self._set_plugins)
 
-        # self.labelSpeed = Label(self,text="Speed (km/Hr)",width=12)
-        # self.labelSpeed.grid(row=0,column=1)
-        # self.labelAmplitude = Label(self,text="Amplitude",width=12)
-        # self.labelAmplitude.grid(row=0,column=2)
-
-        # self.textSpeed = Entry(self,width=12)
-        # self.textSpeed.grid(row=1,column=1)
-        # self.textAmplitude = Entry(self,width=12)
-        # self.textAmplitude.grid(row=1,column=2)
-
-        # self.textAmplitude.insert(0, "1.0")
-        # self.textSpeed.insert(0, "1.0")
-        # self.v = 1.0
-        # self.A = 1.0
-
-        # self.buttonPlot = Button(self,text="Print",command=self.Plot,width=12)        
-        # self.buttonPlot.grid(row=2,column=1)
-        self.buttonClear = Button(self,text="Print",command=self.Clear,width=12)
-        self.buttonClear.grid(row=2,column=1)
-        self.buttonClear.bind(lambda e:self.Clear)
-
-        # Dropdown menu
-        self.labelDropdown = Label(self, text="Kinematics", width=12)
-        self.labelDropdown.grid(row=3, column=1)
-
-        self.options = ['Option 1', 'Option 2', 'Option 3']
-        self.selected_option = tk.StringVar(value=self.options[0])
-        self.dropdown = ttk.Combobox(self, textvariable=self.selected_option, values=self.options, state="readonly")
-        self.dropdown.grid(row=3, column=2)
+        self._init_plugins()
 
         tk.Label(self,text="SHM Simulation").grid(column=0, row=3)
 
         self.canvas = FigureCanvasTkAgg(self.vis_scene.figure, master=self)
         self.canvas.get_tk_widget().grid(column=0,row=4)
 
-        self.ani = FuncAnimation(self.vis_scene.figure, self.update, init_func=self.vis_scene.init_space, frames=np.arange(0, 10000, 1), interval=self.sim_interval, blit=False)
+        self.ani = FuncAnimation(self.vis_scene.figure, self._update, init_func=self.vis_scene.init_space, frames=np.arange(0, 10000, 1), interval=self.sim_interval, blit=False)
 
     def _publish_joints(self):
         self.joint_publisher.publish([self.curr_joints[0], self.curr_joints[1]])
         print(f"Curr Joint Position: {self.curr_joints}")
+
+    def _read_plugin_data(self):
+        shared_plugin_path = os.path.expanduser(self.PLUGIN_FILE_PATH)
+        yaml_file_path = os.path.join(shared_plugin_path, "plugins.yml")
+
+        with open(yaml_file_path, "r") as yaml_file:
+            self.plugin_dict = yaml.safe_load(yaml_file)
+
+    def _init_plugins(self):
+        row_inc = 0
+        for group_name, plugins in self.plugin_dict.items():
+            plugin = PluginOption(self, group_name, plugins)
+            plugin.make_label(row=3+row_inc, col=1)
+            plugin.make_drop_down(row=3+row_inc, col=2)
+            self.plugin_menu[group_name] = plugin
+            row_inc += 1
+
+        self._set_plugins()
 
 
 if __name__ == "__main__":
@@ -159,7 +187,7 @@ if __name__ == "__main__":
     DH_params = DH_parameters(theta, a, d, alpha)
 
     root = tk.Tk()
-    root.geometry("1000x700")
+    root.geometry("1200x700")
     app = SimApp(DH_params, root)
     tk.mainloop()
 
