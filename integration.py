19610706f9143fd20cb246e472f1e9bf3fea2cc2
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-21 01:18:20 -0800

    [TESTING] Updates to req files

--- integration.py
+++ integration.py
@@ -8,15 +8,15 @@ robot = RobotAPI(yml_path)
 path1 = [WayPoint((0.24, -0.24), 0.3)]
 robot.execute_path(path1)
 
-path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
+path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.04, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
 robot.execute_path(path)
 
 
-path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, -0.24), 0.2), WayPoint((0.09, 0.24), 0.6)]
-robot.execute_path(path)
+# path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, -0.24), 0.2), WayPoint((0.09, 0.24), 0.6)]
+# robot.execute_path(path)
 
-path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
-robot.execute_path(path)
+# path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
+# robot.execute_path(path)
 
 
 
