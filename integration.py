0bf4c74a4fd2ff9ec56fbd695089d3792ac946ae
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2023-12-27 22:26:05 -0800

    [TESTING] Updated to testing files

--- integration.py
+++ integration.py
@@ -5,11 +5,14 @@ from robotica_datatypes.path_datatypes.waypoint import WayPoint
 yml_path = "/home/drojas/robot_arm/robotica/2_link_robot.yml"
 robot = RobotAPI(yml_path)
 
-path1 = [WayPoint((0.24, -0.24), 0.3)]
-robot.execute_path(path1)
+robot.plan_and_execute((0.54, 0.14))
+robot.plan_and_execute((0.1, 0.24))
+robot.plan_and_execute((0.2, -0.14))
+# path1 = [WayPoint((0.4, 0.14), 0.3)]
+# robot.execute_path(path1)
 
-path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.04, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
-robot.execute_path(path)
+# path = [WayPoint((0.5, 0.0), 0.4), WayPoint((0.1, 0.24), 1.8), WayPoint((0.49, 0.24), 0.6)]
+# robot.execute_path(path)
 
 
 # path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, -0.24), 0.2), WayPoint((0.09, 0.24), 0.6)]
@@ -17,8 +20,6 @@ robot.execute_path(path)
 
 # path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
 # robot.execute_path(path)
-
-
 
 
 
