1979be5091c2c7d123aebc30a339a5114159f698
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2024-01-26 22:10:48 -0800

    [TESTING] Update testing files

--- integration.py
+++ integration.py
@@ -7,19 +7,8 @@ robot = RobotAPI(yml_path)
 
 robot.plan_and_execute((0.54, 0.14))
 robot.plan_and_execute((0.1, 0.24))
-robot.plan_and_execute((0.2, -0.14))
-# path1 = [WayPoint((0.4, 0.14), 0.3)]
-# robot.execute_path(path1)
+robot.plan_and_execute((0.4, -0.14))
 
-# path = [WayPoint((0.5, 0.0), 0.4), WayPoint((0.1, 0.24), 1.8), WayPoint((0.49, 0.24), 0.6)]
-# robot.execute_path(path)
-
-
-# path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, -0.24), 0.2), WayPoint((0.09, 0.24), 0.6)]
-# robot.execute_path(path)
-
-# path = [WayPoint((0.35, 0.0), 0.4), WayPoint((0.24, 0.24), 0.2), WayPoint((0.49, 0.24), 0.6)]
-# robot.execute_path(path)
 
 
 
