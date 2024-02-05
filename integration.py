7134b0d75930a6f42d87d5bc2b328f0ca151f335
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2024-02-04 21:08:32 -0800

    [TESTING] Add env files

--- integration.py
+++ integration.py
@@ -3,11 +3,12 @@ from robotica_datatypes.path_datatypes.waypoint import WayPoint
 
 
 yml_path = "/home/drojas/robot_arm/robotica/2_link_robot.yml"
-robot = RobotAPI(yml_path)
+env_path = "/home/drojas/robot_arm/robotica/env.yml"
+robot = RobotAPI(yml_path, env_yml_file=env_path)
 
-robot.plan_and_execute((0.54, 0.14))
+robot.plan_and_execute((0.05, 0.54))
 robot.plan_and_execute((0.1, 0.24))
-robot.plan_and_execute((0.4, -0.14))
+robot.plan_and_execute((0.25, 0.5))
 
 
 
