8f04f20d12ecad4b150017fad897a1d6fc41a32b
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2024-02-14 20:05:21 -0800

    [Cleanup] Delete robotica_functions.sh

--- robotica_core/build.sh
+++ robotica_core/build.sh
@@ -10,18 +10,6 @@ if [ -z "$ROBOTICA_CORE_DIR" ]; then
     echo "Error: The environment variable ROBOTICA_CORE_DIR is not set."
 fi
 
-# Check if the script has been sourced in ~/.bashrc
-if ! grep -q "source $ROBOTICA_CORE_DIR/robotica_functions.sh" ~/.bashrc; then
-    # If not, append the source line to ~/.bashrc
-    echo -e "\n# Source custom robotica functions" >> ~/.bashrc
-    echo "source $ROBOTICA_CORE_DIR/robotica_functions.sh" >> ~/.bashrc
-    echo "Custom functions sourced in ~/.bashrc. Restart your shell or run 'source ~/.bashrc' to apply."
-else
-    # If already sourced, print a message
-    echo "Custom functions are already sourced in ~/.bashrc. No changes made."
-fi
-
-
 # Run the setup.py sdist command
 python setup.py sdist
 
